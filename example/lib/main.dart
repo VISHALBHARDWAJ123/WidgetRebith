1c9e04824007d4349a98ccf768f002e0931a39ac
VISHALBHARDWAJ123 <vishal.matrid2380@gmail.com>
2022-08-01 18:28:10 +0530

    test

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -95,6 +95,12 @@ class SplashScreen extends StatefulWidget {
   @override
   State<SplashScreen> createState() => _SplashScreenState();
 }
+class SplashScreenDummy extends StatefulWidget {
+  const SplashScreenDummy({Key? key}) : super(key: key);
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
 
 class _SplashScreenState extends State<SplashScreen> {
   @override
